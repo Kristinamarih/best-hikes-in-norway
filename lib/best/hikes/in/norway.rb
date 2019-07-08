2888d50fb0f4ca467559177d9f11cb316166b7c3
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-08 15:03:53 +0000

    first commit

---
+++ lib/best/hikes/in/norway.rb
@@ -0,0 +1,12 @@
+require "best/hikes/in/norway/version"
+
+module Best
+  module Hikes
+    module In
+      module Norway
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/best/hikes/in/norway/version.rb
@@ -0,0 +1,9 @@
+module Best
+  module Hikes
+    module In
+      module Norway
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
