4a81cb395e810f3cf15b0138e64b1701b3c1f2c3
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-09 14:29:55 +0000

    added environment file in lib

---
+++ lib/best_hikes_in_norway.rb
@@ -0,0 +1,8 @@
+require "nokogiri"
+require "open-uri"
+require "pry"
+
+require_relative "../lib/best_hikes_in_norway/cli.rb"
+require_relative "../lib/best_hikes_in_norway/hikes.rb"
+require_relative "../lib/best_hikes_in_norway/scraper.rb"
+require_relative "../lib/best_hikes_in_norway/version.rb"
--- lib/best_hikes_in_norway/cli.rb
+++ lib/best_hikes_in_norway/cli.rb
@@ -1,19 +1,18 @@
 class BestHikes::CLI 
   
   def call 
-    list_hikes
+    puts "Best Hikes in Norway"
+    display_hikes
     hike_info
   end
   
-  def list_hikes 
-    puts "Best Hikes in Norway:"
+  def display_hikes
     BestHikes::Hikes.new_from_page(BestHikes::Scraper.get_page)
   end
   
-  def display_hikes
-  
   def hike_info
     puts "Please enter the number of the hike you'd like more information on."
+    display_hikes
     input = gets.strip.to_i 
     puts "Hikes:"
     
