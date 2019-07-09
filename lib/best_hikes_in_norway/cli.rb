6f437e3cae7970b1b415bf436a700ce95540d5d9
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-09 13:43:00 +0000

    list_hikes method in CLI class

--- lib/best_hikes_in_norway/cli.rb
+++ lib/best_hikes_in_norway/cli.rb
@@ -7,8 +7,10 @@ class BestHikes::CLI
   
   def list_hikes 
     puts "Best Hikes in Norway:"
-    BestHikes::Scraper.new
+    BestHikes::Hikes.new_from_page(BestHikes::Scraper.get_page)
   end
+  
+  def display_hikes
   
   def hike_info
     puts "Please enter the number of the hike you'd like more information on."
