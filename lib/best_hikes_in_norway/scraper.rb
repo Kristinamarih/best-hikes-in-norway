97f196bb6d52636f290590ff0e0f055c73544e31
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-09 12:54:20 +0000

    building make_hike method in scraper class

--- lib/best_hikes_in_norway/scraper.rb
+++ lib/best_hikes_in_norway/scraper.rb
@@ -5,18 +5,18 @@ require 'pry'
 class BestHikes::Scraper
   
   def get_page 
-    Nokogiri::HTML(open(https://outtt.com/en/guides/225/12-best-hikes-norway))
+    Nokogiri::HTML(open("https://outtt.com/en/guides/225/12-best-hikes-norway"))
   end
   
   def get_hikes 
-    self.get_page.css()
+    self.get_page.css("div.column.is-one-third-tablet")
   end
   
   def make_hikes 
     self.get_hikes.each do |hike|
       hike = Hike.new 
-      hike.name = 
-      hike.location = 
+      hike.name = hike.css(".sort-title").text
+      hike.location = hike.css("a").attribute("href").value
       hike.duration = 
       hike.difficulty = 
       hike.description = 
